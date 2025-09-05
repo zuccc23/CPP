28310c1329752acd4f0921601cd220d719d9be78
zuccc23 <daniyaahmane@gmail.com>
2025-09-05 17:39:14 +0200

    stuff

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-using namespace std;
+#include <stdio.h>
+// using namespace std;
+
+namespace	TestSpace{
+	int	num_var = 1;
+}
+
+namespace	SecondTestSpace{
+	int	num_var = 3;
+}
 
 int main() {
-	cout << "Hello World!";
+	// std::cout << "Test 1: \n";
+	printf("Test 1: %d\n", TestSpace::num_var);
+	printf("Test 2: %d\n", SecondTestSpace::num_var);
 	return 0;
 }
